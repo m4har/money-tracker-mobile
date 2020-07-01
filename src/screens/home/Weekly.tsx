1f068e034584e9d409203d210ce131701672d673
mahardicka nurachman <mahardicka404@gmail.com>
2020-07-01 21:59:49 +0700

    add money screens

---
+++ src/screens/home/Weekly.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import {View, Text} from 'react-native';
+
+const Weekly = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Weekly</Text>
+    </View>
+  );
+};
+
+export default Weekly;
